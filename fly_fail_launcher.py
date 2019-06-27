daf9ecde71e2a9e1c00fa4a2ca3ba9e45500059a
Grace Woolson <gwoolson@hamilton.edu>
2019-06-27 11:27:01 -0400

    fly

--- fly_fail_launcher.py
+++ fly_fail_launcher.py
@@ -113,47 +113,6 @@ for run in range(0, number_runs):
 """ % (title_string, run, full_command, service_tag)
 
 
-for run in range(0, number_runs):
-    intro_command = "echo Starting run; cd %scollatz-numbers/; export PYTHONHOME=/usr; export PYTHONPATH=$PYTHONPATH:/opt/sdsc/lib:/opt/sdsc/lib/python2.6/site-packages:/usr/lib64/python26.zip:/usr/lib64/python2.6/plat-linux2:/usr/lib64/python2.6/lib-tk:/usr/lib64/python2.6/lib-old:/usr/lib64/python2.6/lib-dynload:/usr/lib/python2.6/site-packages/setuptools-0.6c11-py2.6.egg-info:/usr/lib64/python2.6:/usr/lib64/python2.6/site-packages:/usr/lib/python2.6/site-packages; \
-    %s%i%s;" % (output_directory, zip_start25, run, zip_end)
-    command = "python fly_failedall.py run%i.csv %scollatz-numbers/data%i.csv;" % (run, output_directory, run)
-    outro_command = "cd /state/partition1/; rm data%i.csv; echo Finished Run" % (run)
-
-    full_command = intro_command + command + outro_command
-
-    alfcode += """    Task -title {%s - run %i} -cmds {
-        RemoteCmd {/bin/sh -c {%s}} -service {%s} -tags {max20}
-    }
-""" % (title_string, run, full_command, service_tag)
-
-for run in range(0, number_runs):
-    intro_command = "echo Starting run; cd %scount-odds/; export PYTHONHOME=/usr; export PYTHONPATH=$PYTHONPATH:/opt/sdsc/lib:/opt/sdsc/lib/python2.6/site-packages:/usr/lib64/python26.zip:/usr/lib64/python2.6/plat-linux2:/usr/lib64/python2.6/lib-tk:/usr/lib64/python2.6/lib-old:/usr/lib64/python2.6/lib-dynload:/usr/lib/python2.6/site-packages/setuptools-0.6c11-py2.6.egg-info:/usr/lib64/python2.6:/usr/lib64/python2.6/site-packages:/usr/lib/python2.6/site-packages; \
-    %s%i%s;" % (output_directory, zip_start27, run, zip_end)
-    command = "python fly_failedall.py run%i.csv %scount-odds/data%i.csv;" % (run, output_directory, run)
-    outro_command = "cd /state/partition1/; rm data%i.csv; echo Finished Run" % (run)
-
-    full_command = intro_command + command + outro_command
-
-    alfcode += """    Task -title {%s - run %i} -cmds {
-        RemoteCmd {/bin/sh -c {%s}} -service {%s} -tags {max20}
-    }
-""" % (title_string, run, full_command, service_tag)
-
-
-for run in range(0, number_runs):
-    intro_command = "echo Starting run; cd %svector-average/; export PYTHONHOME=/usr; export PYTHONPATH=$PYTHONPATH:/opt/sdsc/lib:/opt/sdsc/lib/python2.6/site-packages:/usr/lib64/python26.zip:/usr/lib64/python2.6/plat-linux2:/usr/lib64/python2.6/lib-tk:/usr/lib64/python2.6/lib-old:/usr/lib64/python2.6/lib-dynload:/usr/lib/python2.6/site-packages/setuptools-0.6c11-py2.6.egg-info:/usr/lib64/python2.6:/usr/lib64/python2.6/site-packages:/usr/lib/python2.6/site-packages; \
-    %s%i%s;" % (output_directory, zip_start19, run, zip_end)
-    command = "python fly_failedall.py run%i.csv %svector-average/data%i.csv;" % (run, output_directory, run)
-    outro_command = "cd /state/partition1/; rm data%i.csv; echo Finished Run" % (run)
-
-    full_command = intro_command + command + outro_command
-
-    alfcode += """    Task -title {%s - run %i} -cmds {
-        RemoteCmd {/bin/sh -c {%s}} -service {%s} -tags {max20}
-    }
-""" % (title_string, run, full_command, service_tag)
-
-
 alfcode += "}\n"
 
 alf_f.writelines(alfcode)
