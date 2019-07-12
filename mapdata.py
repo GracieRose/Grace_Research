350d2a4e15a448de0bdee9035048247063a534ad
Grace Woolson <gwoolson@hamilton.edu>
2019-07-12 15:28:59 -0400

    map_all is accurate

--- mapdata.py
+++ mapdata.py
@@ -2,6 +2,39 @@ import os
 import sys
 import csv
 
+def find_funcs(directories):
+
+	all_funcs = []
+
+	outputFilePrefix = "mapdata"
+	outputFileSuffix = ".csv"
+
+	for outputDirectory in directories:
+
+	    if outputDirectory[-1] != '/':
+	        outputDirectory += '/'
+	    dirList = os.listdir(outputDirectory)
+
+	    z = 0
+	    while (outputFilePrefix + str(z) + outputFileSuffix) in dirList:
+
+	        fileName = (outputFilePrefix + str(z) + outputFileSuffix)
+	        f = open(outputDirectory + fileName)
+	        z += 1
+
+	        funcs = []
+
+	        for line in f:
+
+	        	if (not line.startswith("Gen ")) and (not (line.startswith("0") or line.startswith("1.0"))):
+	        		funcs = line[:-1].split(",")
+
+	        		for item in funcs:
+	        			if not item in all_funcs:
+	        				all_funcs.append(item)
+
+	return all_funcs
+
 
 # Make a verison of this that will run on HPC
 #directories = ["/home/gwoolson/research/gwoolson/Grace_Research/mapdata/checksum"]
@@ -30,6 +63,7 @@ else:
 destfile = open(destination, mode="wb")
 destwriter = csv.writer(destfile)
 
+all_funcs = find_funcs(directories)
 all_gens = []
 
 for outputDirectory in directories:
@@ -39,7 +73,6 @@ for outputDirectory in directories:
     if outputDirectory[-1] != '/':
         outputDirectory += '/'
     dirList = os.listdir(outputDirectory)
-
 
     z = 0
     while (outputFilePrefix + str(z) + outputFileSuffix) in dirList:
@@ -61,56 +94,54 @@ for outputDirectory in directories:
 
         	elif line.startswith("0") or line.startswith("1.0"):
         		freqs = line[:-1].split(",")
-        		#print "freqs: ", freqs
-        		#print
 
         	else:
         		funcs = line[:-1].split(",")
 
         	if freqs == line[:-1].split(","):
 
-        		for i in range(0, len(funcs)):
+        		for i in range(0, len(all_funcs)):
 
-        			func = funcs[i]
+        			func = all_funcs[i]
 
-        			frequency = float(freqs[i])
+        			if func in funcs:
+        				funcindex = funcs.index(func)
+	        			frequency = float(freqs[funcindex])
+	        		else:
+	        			frequency = 0.0
 
         			if func in all_gens[gen]:
         				all_gens[gen][func].append(frequency)
         			else:
         				all_gens[gen][func] = [frequency]
 
-gencount = 0
+
+header = True
 funcs_printed = []
 to_print = []
 for generation_map in all_gens:
 
-	funcslist = []
+	#use all_funcs instead
+	#funcslist = []
 	freqslist = [] 
 	
-	for function in generation_map:
+	for function in all_funcs:
+
+		#debugging
+		#print all_funcs[counter]
+		#print all_gens[0][all_funcs[0]]
+		#print sum(all_gens[0][all_funcs[counter]]) / len(all_gens[0][all_funcs[counter]])
 
 		frequencies = generation_map[function]
 
 		avg_freq = sum(frequencies) / len(frequencies)
-		#print frequencies
-		
-		funcslist.append(function)
+
 		freqslist.append(avg_freq)
-		
-		"""if function == "exec_do*whi":
-			print frequencies, len(frequencies)
-			print avg_freq
-			print"""
 
-		genprint = "Gen %i" % gencount
-
-	if gencount == 0:
-		destwriter.writerow(funcslist)
-	#funcs_printed = funcslist
-
+	if header:
+		destwriter.writerow(all_funcs)
+		header = False
 
 	destwriter.writerow(freqslist)
-	gencount += 1
 
 #print all_gens
---
+++ mapdata/replace-space-with-newline/map.py
@@ -0,0 +1,205 @@
+import sys
+import csv
+
+def check_if_constant(instruction):
+
+    is_constant = True
+
+    instr_types = ["in", "autoconstructive", "boolean", "char", "code", "environment", "exec", "float", "genome", "gtm", "integer", "noop", "print", "return", "string", "vector", "zip"]
+
+    for category in instr_types:
+        if instruction.startswith(category):
+            is_constant = False
+            break
+
+    #print instruction, is_constant
+    return is_constant
+
+
+def deparenthasize(lst):
+
+    newlst = []
+        
+    for i in range(0, len(lst)):
+
+        word = lst[i]
+        #print "Before: %s" % word
+        done = False
+
+        while not done:
+            if word == "(":
+                word = "" 
+            elif len(word) > 0 and word[0] == "(":
+                word = word[1:]
+            if len(word) > 0 and word[-1] == ")":
+                word = word[:-1]
+
+            if len(word) > 0:
+                if word[0] == "(" or word[-1] == ")":
+                    done = False
+                else:
+                    done = True
+            else:
+                done = True
+
+        if not word == "":
+            newlst.append(word)
+
+        #print "After: %s" % word
+
+    return newlst
+
+
+
+def debracket(lst):
+    newlist = []
+
+    for word in lst:
+
+        newword = word
+
+        if word[0] == "{":
+            newword = newword[1:]
+
+        if word[-1] == "}":
+            newword = newword[:-1]
+
+        newlist.append(newword)
+
+    return newlist
+
+
+def find_funcs_and_simplify(genome, freqs):
+
+    funcslist = []
+
+    for i in range(0, len(genome) - 1):
+
+        if genome[i] == ":instruction":
+            
+            func = genome[i+1]
+
+            if func[-1] == ",":
+                func = func[:-1]
+
+            is_constant = check_if_constant(func)
+            if (not is_constant) and (not func in funcslist):
+
+                if func in freqs:
+                    freqs[func] += 1
+                else:
+                    freqs[func] = 1
+                funcslist.append(func)
+
+
+    return freqs
+
+
+
+def find_path(directory):
+    #ex: takes /home/gwoolson/csvdata/collatz-numbers/data2.csv -> /home/gwoolson/csvdata/collatz-numbers/
+
+    if directory[-10] == "/":
+        path = directory[:-9]
+    else:
+        path = directory[:-10]
+
+    return path
+
+
+
+def map():
+
+    if len(sys.argv) > 1:
+        outputFile = sys.argv[1]
+        isdonepath = find_path(outputFile)
+
+        if ("double-letters" in outputFile) or ("string-lengths-backwards" in outputFile) or ("vector-average" in outputFile):
+            tests = -101
+
+        elif ("count-odds" in outputFile) or ("negative-to-zero" in outputFile) or ("scrabble-score" in outputFile) or ("checksum" in outputFile) or ("syllables" in outputFile) or ("replace-space-with-newline" in outputFile):
+            tests = -201
+
+    else:
+        print "Please provide a file to read"
+        exit(1)
+
+    if len(sys.argv) > 2:
+        destination = sys.argv[2]
+
+        donenum = destination[7:]
+        donenum = int(donenum[:-4])
+
+    else:
+        print("please provide a destination file in the format <filename>.csv")
+        exit(1)
+
+    isdonefile = isdonepath + "donetest/done%i.csv" % donenum
+    print isdonefile
+
+    destfile = open(destination, mode="w")
+    destwriter = csv.writer(destfile)
+
+    f = open("/state/partition1/data%i.csv" % donenum)
+
+    gen = 0
+    gencount = 0
+    freqs = {}
+    destwriter.writerow(["Gen 0"])
+
+    for line in f:
+        if not line.startswith("uuid"):
+
+            line = line.split(",")
+            this_gen = int(line[1])
+
+            if this_gen != gen: 
+
+                funcslist = []
+                freqslist = []
+                for key in freqs:
+                    funcslist.append(key)
+                    #turns the frequency into a percentage
+                    freqslist.append(freqs[key]/float(gencount))
+
+                    #this ensures that the lists will always be in the same order
+                    freqs[key] = 0
+
+                if gen == 0:
+                    destwriter.writerow(funcslist)
+
+                destwriter.writerow(freqslist)
+
+                destwriter.writerow(["Gen %i" % this_gen])
+                gen = this_gen
+                gencount = 0  
+
+            gencount += 1  
+
+            if "replace-space-with-newline" in outputFile:
+                line = ",".join(line[7:tests])
+                genome_start = line.index(":instruction") - 3
+                line = line[genome_start:]
+            else:
+                line = ",".join(line[7:tests])
+
+            line = debracket(deparenthasize(line[1:-1].split()))
+
+            freqs = find_funcs_and_simplify(line, freqs)
+
+    funcslist = []
+    freqslist = []
+    for key in freqs:
+        funcslist.append(key)
+        freqslist.append(freqs[key]/float(gencount))
+
+    #destwriter.writerow(funcslist)
+    destwriter.writerow(freqslist)
+
+    is_finished = open(isdonefile, mode="w")
+
+def main():
+    map()
+
+if __name__ == '__main__':
+    main()
